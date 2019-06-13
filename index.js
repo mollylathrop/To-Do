8700769ff2a95f118f997e926c8af9c7443ddbaa
Molly Lathrop <mollylathrop@Mollys-MacBook-Pro.local>
2019-06-13 08:45:56 -0500

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var cardContainer = $('.card-container');
 var submit = $('.submit-btn');
-var currentPlan = localStorage.getItem("name-card");
+var currentPlan = JSON.parse(localStorage.getItem('plan')) || [];
+
+
+
+function appendCurrentPlan() {
+  currentPlan.forEach(function(plan) {
+    cardContainer.append(`
+      <article class="name-card"><input type= "checkbox" class= "checkbox">
+        ${plan.exercise}: ${plan.sets} sets of ${plan.reps} at ${plan.weight} lbs
+      </article>
+    `);
+    var checkbox = $('.checkbox');
+    checkbox.on("click", remove)
+  })
+}
+
+appendCurrentPlan();
 
 submit.on("click", appendPlan);
 
-function appendPlan() {
+function appendPlan(e) {
+  e.preventDefault();
   var exercise = $('.exercise').val();
   var sets = $('.sets').val();
   var reps = $('.reps').val();
@@ -24,14 +41,13 @@ function appendPlan() {
   $('.reps').val("");
   $('.weight').val("");
 
-  storePlan(currentPlan);
+  currentPlan.push({exercise: exercise, sets: sets, reps: reps, weight: weight});
+  var currentPlanString = JSON.stringify(currentPlan);
+  localStorage.setItem('plan', currentPlanString);
+  console.log(localStorage);
 }
 
 function remove(){
 event.target.parentNode.remove();
- }
-
- function storePlan(e) {
-   e.preventDefault();
-   localStorage.setItem('name-card', name-card);
+localStorage.removeItem('plan');
  }
